00a14f7e52da2e9cceb9d6d90886163f31e9eebe
Girish Ramnani <girish.ramnani@crestdatasys.com>
2018-04-06 15:16:36 +0530

    added ci steps and dockerfile

---
+++ ci/scripts/publish.sh
@@ -0,0 +1,9 @@
+if [ -z "$CI_NAME" ]; then
+    echo "Skipping publishing since not running in CI"
+    exit 0
+fi
+
+packages_bucket="cloudworks-terraform-providers"
+artifacts_dir=$1
+
+aws s3 sync $artifacts_dir s3://$packages_bucket --exclude "*" --include "*.zip"
